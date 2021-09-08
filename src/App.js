791097725c7ad643eacc33d7dd68f20f55400b6f
Ariel Solla <arielsolla@gmail.com>
2021-09-08 18:53:33 -0300

    NavBar con Style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 
+const color ={
+  backgroundColor: 'gray',
+  width: '100%',
+  height: '50vw',
+};
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'Black',
+  color: 'white',
+  margin:'0',
+  padding: '20px 0px 20px 0px'
+};
+
+
 function App() {
   return (
-    <NavBar/>
-    
+    <>
+    <div style={color}>  
+      <div>
+        <h1 style={style}>Gaming E-Commerce</h1>
+      </div>
+      <div>
+        <NavBar/>
+      </div>
+    </div>
+    </>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,30 @@
-const NavBar = () =>{
+const divStyle={
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: 'beige',
+    color: 'black',
+}
+const ulStyle={
+    display: 'flex',
+    listStyleType: 'none',
+    width: '50vw',
+    justifyContent: 'space-evenly',
+    padding: '0',
+    margin: '20px 0px 20px 0px',
+}
+
+const NavBar = () =>{    
     return(
-        <div>
-            <p>Inicio</p>
-            <p>PLAYSTATION</p>
-            <p>XBOX One</p>
-            <p>SWITCH</p>
-            <p>WII</p>
-            <p>PC</p>
+        <div style={divStyle}>
+            <ul style={ulStyle}>
+                <li>INICIO</li>
+                <li>PLAYSTATION</li>
+                <li>XBOX One</li>
+                <li>SWITCH</li>
+                <li>WII</li>
+                <li>PC</li>
+            </ul>
         </div>
     )
 }
