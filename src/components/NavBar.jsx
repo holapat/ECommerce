28db62f06e6d43576ec2a1369c356b89cd7ffe3a
Ariel Solla <arielsolla@gmail.com>
2021-09-08 16:50:11 -0300

    NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () =>{
+    return(
+        <div>
+            <p>Inicio</p>
+            <p>PLAYSTATION</p>
+            <p>XBOX One</p>
+            <p>SWITCH</p>
+            <p>WII</p>
+            <p>PC</p>
+        </div>
+    )
+}
+
+export default NavBar
